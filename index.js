71f61c97e76fc79f4454cf41cca2d1031be6bcff
Edgar Talledos <onimenotsuki@gmail.com>
2019-08-06 12:02:59 -0500

    Create small api for toggle power of LED with arduino

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const five = require('johnny-five');
 const board = new five.Board();
 
 board.on('ready', () => {
   const led = new five.Led(13);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
 
   app.get('/powerOn', (req, res) => {
     led.on();
@@ -12,6 +17,23 @@ board.on('ready', () => {
 
   app.get('/powerOff', (req, res) => {
     led.off();
+  });
+
+  app.post('/action', (req, res) => {
+    switch (req.body.power) {
+    case 'on':
+      led.on();
+      res.send('Led encendido');
+      break;
+    case 'off':
+      led.off();
+      res.send('Led apagado');
+      break;
+    default:
+      led.on();
+      res.send('Led encendido');
+      break;
+    }
   });
 
   app.listen(3000, () => console.log('Running server in port: 3000'));
