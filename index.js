487d21f7955b9cdd2efd030738d2409d3448072e
Edgar Talledos <onimenotsuki@gmail.com>
2019-08-06 11:55:41 -0500

    Add sample application with express

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const express = require('express');
+const app = express();
 const five = require('johnny-five');
 const board = new five.Board();
 
 board.on('ready', () => {
-  var led = new five.Led(13);
-  led.blink(500);
+  const led = new five.Led(13);
+
+  app.get('/powerOn', (req, res) => {
+    led.on();
+  });
+
+  app.get('/powerOff', (req, res) => {
+    led.off();
+  });
+
+  app.listen(3000, () => console.log('Running server in port: 3000'));
 });
